ed89b0f44bd96336eea7a7772e757795646bda41
aiqinxuancai <aiqinxuancai@163.com>
2018-06-29 00:10:39 +0800

    添加项目文件。

---
+++ RamMonitor/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using Flurl.Http;
+using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RamMonitor
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var ret =  Services.PowerShell.RunScriptText("docker -h");
+            //
+            //HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell\
+            //ExecutionPolicy = RemoteSigned
+
+            test();
+        }
+
+        public void test()
+        {
+
+
+            //var responseString = await "https://api.eosnewyork.io/v1/chain/get_table_rows"
+            //.PostUrlEncodedAsync(new { json = "true", code = "eosio", scope = "eosio", table = "global" })
+            //.ReceiveString();
+
+            //HttpClient client = new HttpClient();
+            //var values = new Dictionary<string, string>
+            //{
+            //   { "json", "true" },
+            //   { "code", "eosio" },
+            //    { "scope", "eosio" },
+            //   { "table", "global" }
+            //};
+            //client.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36");
+            //var content = new FormUrlEncodedContent(values);
+
+            //var response = await client.PostAsync("https://api.eosnewyork.io/v1/chain/get_table_rows", content);
+
+            //var responseString2 = await response.Content.ReadAsStringAsync();
+
+
+
+            var data = "https://www.feexplorer.io/EOS_RAM_price".GetStringAsync().Result;
+
+            /*
+                <h6>Current <span style="color:#734ce3;">RAM</span> Price</h6>
+                <p style="font-size:120%;">0.11985765 EOS per kb</p>
+                </div>
+            */
+
+
+
+
+            //Regex reg = new Regex(@"RAM</span> Price</h6>\r\n<p style=""font-size:120%;"">([0-9]*\.[0-9]*) EOS per");
+            Regex reg = new Regex(@"([0-9]*\.[0-9]*) EOS per");
+            var match = reg.Match(data);
+            Debug.WriteLine(data);
+            var groups = match.Groups;
+            foreach (Group item in groups)
+            {
+                Debug.WriteLine(item.ToString());
+            }
+
+           
+
+
+
+        }
+
+
+
+    }
+}
---
+++ RamMonitor/Services/PowerShell.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management.Automation.Remoting;
+using System.Management.Automation.Runspaces;
+using System.Collections.ObjectModel;
+
+namespace RamMonitor.Services
+{
+    public class PowerShellResult
+    {
+        public bool HasError { set; get; }
+        public string ResultText { set; get; }
+        public string ErrorText { set; get; }
+    }
+
+
+
+
+    public class PowerShell
+    {
+        public static PowerShellResult RunCleosCode(string text)
+        {
+            return RunScriptText(@"docker exec -i keosd /opt/eosio/bin/cleos --wallet-url http://127.0.0.1:8900 -u http://mainnet.genereos.io " + text);
+        }
+
+        public static PowerShellResult RunScriptText(string text)
+        {
+            // create Powershell runspace
+            Runspace runspace = RunspaceFactory.CreateRunspace();
+            // open it
+            runspace.Open();
+            // create a pipeline and feed it the script text
+            Pipeline pipeline = runspace.CreatePipeline();
+
+            string scriptText = text;
+
+            pipeline.Commands.AddScript(scriptText);
+            // add an extra command to transform the script output objects into nicely formatted strings
+            // remove this line to get the actual objects that the script returns. For example, the script
+            // "Get-Process" returns a collection of System.Diagnostics.Process instances.
+            //pipeline.Commands.Add("Out-String");
+            // execute the script
+            System.Collections.ObjectModel.Collection<PSObject> results = pipeline.Invoke();
+            // close the runspace
+            runspace.Close();
+            // convert the script result into a single string
+
+            PowerShellResult powerShellResult = new PowerShellResult();
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (PSObject obj in results)
+            {
+                stringBuilder.AppendLine(obj.ToString());
+            }
+            powerShellResult.ResultText = stringBuilder.ToString();
+
+
+
+            Collection<object> errors = pipeline.Error.ReadToEnd(); // Streams property is not available
+            StringBuilder stringBuilderError = new StringBuilder();
+            if (errors != null && errors.Count > 0)
+            {
+                foreach (PSObject er in errors)
+                {
+                    stringBuilderError.AppendLine(er.ToString());
+                }
+                powerShellResult.ErrorText = stringBuilder.ToString();
+                powerShellResult.HasError = true;
+            }
+
+            return powerShellResult;
+        }
+    }
+}
